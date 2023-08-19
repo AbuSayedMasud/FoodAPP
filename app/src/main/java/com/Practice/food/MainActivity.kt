57b53b273d6fe6bd69ff3d0937de170c6b07ad4f
AbuSayedMasud <68724752+AbuSayedMasud@users.noreply.github.com>
2023-08-19 11:29:31 +0600

    first commit

---
+++ app/src/main/java/com/Practice/food/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.Practice.food
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.ContextCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            window.statusBarColor = ContextCompat.getColor(this, R.color.primary)
+            window.navigationBarColor=ContextCompat.getColor(this,R.color.primary)
+        }
+
+    }
+}
